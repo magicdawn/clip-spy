0241b3d28f90995756daf2f75c4cd3c45b82feba
magicdawn <magicdawn@qq.com>
2022-01-20 04:17:44 +0800

    chore: update demo

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -2,8 +2,8 @@
 
 import addon from '../addon'
 
-console.log(addon.get('public.utf8-plain-text'))
+console.log(addon.macGet('public.utf8-plain-text'))
 
-console.log(addon.set('some.custom.type', Buffer.from('Hello World')))
-console.log(addon.get('some.custom.type'))
+console.log(addon.macSet('some.custom.type', Buffer.from('Hello World')))
+console.log(addon.macGet('some.custom.type'))
 
